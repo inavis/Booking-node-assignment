9df0c13f869114f153577a389e6dcb0e56f2c2d6
Sivani Saravanan <rednaz.inavis@gmail.com>
2022-01-19 20:34:09 +0530

    added cors

--- booking.js
+++ booking.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors")
 
 const app = express();
 const PORT =9000;
 
 //middleware
 app.use(express.json())
+app.use(cors())
 
 const rooms=[
     {
